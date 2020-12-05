d8a2af36d97ec985e4b80091cf69b061012b5bb9
denyncrawford <kiquerangelb@gmail.com>
2020-12-05 02:08:50 -0300

    new screens

--- src/render/components/structure/PageCenter.vue
+++ src/render/components/structure/PageCenter.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex justify-center items-center flex-col min-h-screen min-w-screen p-10 px-16">
+  <div class="flex justify-center text-center items-center flex-col min-h-screen min-w-screen p-10 px-16">
     <div>
       <h1 class="w-96 rounded py-1 px-2 inline text-back uppercase mb-5 bg-main-color">{{ title }}</h1>
-      <p class="text-3xl mt-5 max-w-xs text-blue-400 uppercase font-thin mb-8">{{ description }}</p>
+      <p class="text-3xl mt-5 mx-auto max-w-xs text-blue-400 uppercase font-thin mb-8">{{ description }}</p>
       <div>
         <slot/>
       </div>
--- src/render/views/Mode.vue
+++ src/render/views/Mode.vue
@@ -1,6 +1,15 @@
 <template>
-  <page title="Selecciona un modo">
-    
+  <page description="Elige como usar esta instancia" title="Selecciona un modo">
+    <div class="grid grid-cols-2 gap-10 w-10/12 mx-auto">
+      <div class="cursor-pointer card group hover:bg-main-color text-left bg-transparent p-5 rounded-xl border border-2 border-gray-400">
+        <h1 class="text-xl uppercase group-hover:text-back text-main-color mb-2">Modo admin</h1>
+        <p class="group-hover:text-back text-gray-400">Utilizar en modo administrador para poder acceder a los recursos de los colaboradores.</p>
+      </div>
+      <div class="cursor-pointer card group hover:bg-main-color text-left bg-transparent p-5 rounded-xl border border-2 border-gray-400">
+        <h1 class="text-xl uppercase group-hover:text-back text-main-color mb-2">Marcador</h1>
+        <p class="group-hover:text-back text-gray-400">Utilizar en modo marcador para poder acceder a un nuevo marcado de turnos.</p>
+      </div>
+    </div>
   </page>
 </template>
 
