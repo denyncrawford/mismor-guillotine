c22f494a17647f208e7d8792fc711b4b48c6915c
denyncrawford <kiquerangelb@gmail.com>
2021-02-26 14:59:51 -0300

    love

--- src/render/views/init/Configure.vue
+++ src/render/views/init/Configure.vue
@@ -5,19 +5,31 @@
         title="Configurar Aplicación"
         description="Esta es la configuración del programa"
       >
-      <div>
-        <h2 class="mb-2 font-bold text-blue-400">Indicar el prefijo del lector</h2>
-        <input v-model="selectedDevice" ref="psw" type="text" placeholder="Prefijo" class="mb-5 text-gray-500 py-2 px-5 bg-transparent rounded border outline-none border-1 border-blue-400">
-        <p class="mb-5 group-hover:text-back text-gray-400">Debe configurar el prefijo en el scanner para utilizarlo.</p>    
-      </div>
-      <div>
-        <h2 class="mb-2 font-bold text-blue-400">Escribe un nombre para la base de datos</h2>
-        <input v-model="database" ref="psw" type="text" placeholder="guillotine (default)" class="mb-5 text-gray-500 py-2 px-5 bg-transparent rounded border outline-none border-1 border-blue-400">  
-      </div>
-      <div class="mt-5">
-        <el-button @click="back" class="bg-back text-red hover:bg-back hover:border-main-color hover:text-main-color">Regresar</el-button>
-        <el-button @click="save" class="bg-main-color text-back hover:text-back" type="primary">Continuar</el-button>
-      </div>
+        <div>
+          <h2 class="mb-2 font-bold text-blue-400">Indicar el prefijo del lector</h2>
+          <input v-model="selectedDevice" ref="psw" type="text" placeholder="Prefijo" class="mb-5 text-gray-500 py-2 px-5 bg-transparent rounded border outline-none border-1 border-blue-400">
+          <p class="mb-5 group-hover:text-back text-gray-400">Debe configurar el prefijo en el scanner para utilizarlo.</p>    
+        </div>
+        <div>
+          <h2 class="mb-5 font-bold text-blue-400">Escribe un nombre para la base de datos</h2>
+          <input v-model="database" ref="psw" type="text" placeholder="guillotine (default)" class="mb-5 text-gray-500 py-2 px-5 bg-transparent rounded border outline-none border-1 border-blue-400">  
+        </div>
+        <div class="mb-5">
+          <el-switch
+            v-model="remote"
+            active-text="Remoto"
+            inactive-text="Local">
+          </el-switch>
+        </div>
+        <transition name="slide3">
+          <div class="mb-5" v-show="remote">
+            <input v-model="host" type="text" placeholder="Introduce el host de la base de datos" class="mb-5 text-white py-2 px-5 bg-transparent rounded border outline-none border-1 border-blue-400">
+          </div>
+        </transition>
+        <div class="mt-5">
+          <el-button @click="back" class="bg-back text-red hover:bg-back hover:border-main-color hover:text-main-color">Regresar</el-button>
+          <el-button @click="save" class="bg-main-color text-back hover:text-back" type="primary">Continuar</el-button>
+        </div>
       </page>
     </div>
 </template>
@@ -32,7 +44,9 @@ export default {
       selectedDevice: "",
       database: "",
       defaultDatabase: "guillotine",
-      isLoading: false
+      isLoading: false,
+      remote: false,
+      host: "mongodb://127.0.0.1:27017"
     }
   },
   components: {
@@ -69,6 +83,16 @@ export default {
 </script>
 
 <style>
+
+.slide3-enter-active {
+   animation: fadeInDown;
+   animation-duration: .2s;
+  }
+
+  .slide3-leave-active {
+    animation: fadeOutUp;
+    animation-duration: .2s;
+  }
 
 .el-select {
   min-width: 266px;
